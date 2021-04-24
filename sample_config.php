ac943b69def6d6eb2b5c3816384160123c0db306
Stephan <spamonia@gmail.com>
2021-04-24 11:07:55 +0200

    Updated the sample configuration.

--- sample_config.php
+++ sample_config.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * For the chat to work on your domain you also got to set the
+ * value for 'ajaxURL' in assets/mychat.js:28.
+ */
 
 /** The name of the database*/
 define( 'DB_NAME', '' );
@@ -15,3 +19,11 @@ define( 'DB_HOST', '' );
 /** Video embed code **/
 define('TWITCH_CHANNEL', '');
 
+/** Polling intervall in seconds */
+define('POLL_INTERVAL', 3);
+
+/** Number of polling cycles till a user is marked as offline */
+define('OFFLINE_CYCLES', 3);
+
+/** Limit number of old messages loaded on inital chat display */
+define('HISTORY_COUNT', 3);
